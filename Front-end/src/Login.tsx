6486fea2e84bfd28ced3407275a1da0194840d12
jameel <63780006+jameel0901@users.noreply.github.com>
2025-06-20 21:17:42 +0530

    Simplify docs assets and add build-docs script

--- Front-end/src/Login.tsx
+++ Front-end/src/Login.tsx
@@ -9,7 +9,7 @@ const Login: React.FC = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const res = await fetch('http://localhost:5000/login', {
+      const res = await fetch('https://livedocs-gool.onrender.com/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })
